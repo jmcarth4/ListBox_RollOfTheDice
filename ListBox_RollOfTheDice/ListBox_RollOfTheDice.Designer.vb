85d298c28ce15c4f3027f014493f5713c0a7785f
jmcarth4@gmail.com <jmcarth4@gmail.com>
2022-04-13 20:57:34 -0600

    Add project files.

---
+++ ListBox_RollOfTheDice/ListBox_RollOfTheDice.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ListBox_RollOfTheDice
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox = New System.Windows.Forms.ListBox()
+        Me.RollDiceButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox
+        '
+        Me.ListBox.FormattingEnabled = True
+        Me.ListBox.ItemHeight = 20
+        Me.ListBox.Location = New System.Drawing.Point(0, 0)
+        Me.ListBox.Name = "ListBox"
+        Me.ListBox.Size = New System.Drawing.Size(756, 324)
+        Me.ListBox.TabIndex = 0
+        '
+        'RollDiceButton
+        '
+        Me.RollDiceButton.Location = New System.Drawing.Point(49, 356)
+        Me.RollDiceButton.Name = "RollDiceButton"
+        Me.RollDiceButton.Size = New System.Drawing.Size(107, 62)
+        Me.RollDiceButton.TabIndex = 1
+        Me.RollDiceButton.Text = "Roll "
+        Me.RollDiceButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(382, 356)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(97, 62)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(225, 356)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(97, 62)
+        Me.ClearButton.TabIndex = 3
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ListBox_RollOfTheDice
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.RollDiceButton)
+        Me.Controls.Add(Me.ListBox)
+        Me.Name = "ListBox_RollOfTheDice"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox As ListBox
+    Friend WithEvents RollDiceButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ClearButton As Button
+End Class
---
+++ ListBox_RollOfTheDice/ListBox_RollOfTheDice.vb
@@ -0,0 +1,85 @@
+﻿'Jessica McArthur
+'RCET0265
+'Spring 2020
+'Roll of the Dice- List box
+'https://github.com/jmcarth4/
+
+Public Class ListBox_RollOfTheDice
+
+    'variables to set data array
+    Dim arrayLength As Integer = 12
+    Dim temp As String
+
+    'variables for easy display of the data
+    Dim seperator As String
+    Dim columnLength As Integer = 1
+    Dim lineLength As Integer = 12
+
+
+    'creates random numbers in an array. 
+    'numbers are those of two rolled dice
+    Sub RollDice()
+        Dim randomNumbers(arrayLength) As Integer
+        Dim currentNumber As Integer
+
+        'count random numbers of range of two dice. (2 to 12)
+        For i = 1 To 1000
+            currentNumber = RandomNumberInRange()
+            randomNumbers(currentNumber) += 1
+        Next
+
+        For j = 2 To UBound(randomNumbers)
+            temp += CStr(randomNumbers(j)).PadLeft(5)
+        Next
+
+        ListBox.Items.Add(temp)
+        Console.WriteLine(temp)
+        temp = ""
+    End Sub
+
+    'Displays the generated data
+    Sub Display()
+        ' write title
+        ListBox.Items.Add((CStr("Roll of the Dice").PadLeft(45)))
+        ListBox.Items.Add((StrDup(85, "-")))
+
+        ' space between numbers
+        For j = 2 To lineLength
+
+            seperator += Str(j).PadLeft(6)
+            Console.WriteLine(seperator)
+        Next
+
+        ListBox.Items.Add(seperator)
+        Console.WriteLine(seperator)
+        seperator = " "
+
+        'divides upper and lower row
+        ListBox.Items.Add((StrDup((85), "-")))
+    End Sub
+
+    ' Function GetRandomInteger() As Integer
+    Function RandomNumberInRange(Optional max% = 10%, Optional min% = 0%) As Integer
+        Dim _max% = max - min
+        If _max < 0 Then
+            Throw New System.ArgumentException("Maximum number must be greater than minimum number")
+        End If
+        Randomize(DateTime.Now.Millisecond)
+        Return CInt(System.Math.Floor(Rnd() * (_max + 1))) + min
+    End Function
+
+    'Buttons to prompt roll, clear box and exit program
+    Private Sub RollDiceButton_Click(sender As Object, e As EventArgs) Handles RollDiceButton.Click
+        Display()
+        RollDice()
+     End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        ListBox.Items.Clear()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+End Class
